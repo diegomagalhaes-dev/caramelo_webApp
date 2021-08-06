18f1a8d9af612f6fa24e4c1b23ab3cebf8eefa0c
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-08-05 23:51:03 -0300

    :sparkles: Create Feeders Page

--- src/Screens/Create Feeder/CreateFeeder.tsx
+++ src/Screens/Create Feeder/CreateFeeder.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import { MapContainer, Marker, TileLayer, useMapEvents } from "react-leaflet";
 import L from "leaflet";
 
 import { PageCreateFeeder } from "./StyCreateFeeder";
@@ -8,6 +8,7 @@ import { FiPlus } from "react-icons/fi";
 
 import mapMarkerImg from "../../Images/markMap.svg";
 import Sidebar from "../../Components/Sidebar Component/Sidebar";
+import useGeolocalization from "../../Utils/useLocalization";
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
@@ -18,63 +19,64 @@ const happyMapIcon = L.icon({
 });
 
 const FeederMap = () => {
+  const location = useGeolocalization();
+  const [position, setPosition] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  function MarkerMap() {
+    useMapEvents({
+      click(event) {
+        const { lat, lng } = event.latlng;
+        setPosition({
+          latitude: lat,
+          longitude: lng,
+        });
+      },
+    });
+
+    return position.latitude !== 0 ? (
+      <Marker
+        position={[position.latitude, position.longitude]}
+        interactive={false}
+        icon={happyMapIcon}
+      />
+    ) : null;
+  }
+
   return (
     <PageCreateFeeder>
       <Sidebar />
       <main>
         <form className="create-feeder-form">
           <fieldset>
-            <legend>Dados</legend>
-            <MapContainer
-              center={[-27.2092052, -49.6401092]}
-              style={{ width: "100%", height: 280 }}
-              zoom={15}
-            >
-              <TileLayer
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-              />
-              <Marker
-                interactive={false}
-                icon={happyMapIcon}
-                position={[-27.2092052, -49.6401092]}
-              />
-            </MapContainer>
-
-            <div className="input-block">
-              <label htmlFor="name">Nome</label>
-              <input id="name" />
+            <legend>Informações do comedouro</legend>
+            <div className="create-feeder-map">
+              {location.loaded && (
+                <MapContainer
+                  center={[location.coordinates.lat, location.coordinates.lng]}
+                  style={{ width: "100%", height: 280 }}
+                  zoom={15}
+                  className="map"
+                >
+                  <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  />
+                  <MarkerMap />
+                </MapContainer>
+              )}
+              <p>
+                No mapa temos sua localização atual! <br /> (caso queira outra,
+                basta reposicionar o marcador)
+              </p>
             </div>
             <div className="input-block">
-              <label htmlFor="about">Sobre</label>
-              <textarea id="name" maxLength={300} />
-            </div>
-            <div className="input-block">
-              <label htmlFor="images">Fotos</label>
               <div className="uploaded-image"></div>
+              <label htmlFor="images">Imagem</label>
               <button className="new-image">
-                <FiPlus size={24} color="#17c0c2" />
+                <FiPlus size={24} color="#F8961E" />
               </button>
-            </div>
-          </fieldset>
-
-          <fieldset>
-            <legend>Visitação</legend>
-            <div className="input-block">
-              <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
-            </div>
-            <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <textarea id="opening_hours" />
-            </div>
-            <div className="input-block">
-              <label htmlFor="open_on_weekends">Atende fim de semana</label>
-              <div className="button-select">
-                <button type="button" className="active">
-                  Sim
-                </button>
-                <button type="button">Não</button>
-              </div>
             </div>
           </fieldset>
           <button className="confirm-button" type="submit">
