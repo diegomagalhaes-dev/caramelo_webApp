4de22cbb83faf270ce56a2dffe637f2932f49e7a
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-08-23 23:53:36 -0300

    :sparkes: Authenticate admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Routes from "./routes";
 
+import { AuthProvider } from "./context/AuthContext";
+
 function App() {
-  return <Routes />;
+  return (
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  );
 }
 
 export default App;
